243ab5813f35b006664b5f4b5bddd19626ff2a9e
Kevin Leonardo Fajardo Soto <kevinfajardosoto12@gmail.com>
2024-05-18 14:46:55 -0600

    Create title_words.py

---
+++ python_crash_course_3rd_edition/title_words.py
@@ -0,0 +1,5 @@
+# Let's assume that the user input will be always String
+# It will capitalize the first letter of each word in the string/input provided.
+
+user_input = input("Type the phrase you want to be a Title\n e.g Typing \"my title\" will return \"My Title\"\n> ")
+print("  " + user_input.title())

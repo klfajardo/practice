3705feaa4c2d15bc4cf04a0cc0f20b78261cf7d0
Kevin Leonardo Fajardo Soto <kevinfajardosoto12@gmail.com>
2024-05-18 14:47:50 -0600

    Create lower_words.py

---
+++ python_crash_course_3rd_edition/lower_words.py
@@ -0,0 +1,6 @@
+# Let's assume that the user input will be always String.
+# Otherwise, it will return an error.
+# It will lowercase all letters in the given string.
+
+user_input = (input("Type the words you want to lowercase\n> "))
+print("  " + user_input.lower())
